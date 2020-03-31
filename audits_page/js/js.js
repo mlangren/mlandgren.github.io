fdf3334a5f668a50516a432f7efae29bdd2bc9fd
m.landgren <45962024+mlangren@users.noreply.github.com>
2020-03-31 11:09:18 +0200

    fixa
    fixade

---
+++ audits_page/js/js.js
@@ -0,0 +1,16 @@
+
+
+
+console.log("test");
+$("a").click(function(){
+  
+  
+  $("main").text("hej");
+  
+});
+$("#start").click(function(){
+  
+  
+  $("main").html("  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris libero tortor, auctor ac iaculis et, dapibus vel erat. Vivamus interdum venenatis arcu vel laoreet. Suspendisse finibus sit amet eros et facilisis. Donec a enim sed mi mollis molestie. Vivamus maximus tempor tellus, sit amet sodales arcu congue non. Quisque lacus augue, tempus aliquet aliquet ac, luctus in metus. Nam bibendum enim dictum, accumsan est vel, lobortis augue. Nam diam mauris, blandit   ");
+  
+});
